1fc879a73621738d19f66c26d9f13bd2be3b36a8
unknown <tizanyb@gmail.com>
2024-07-08 20:38:22 +0700

    aaa

--- Server/catetan.js
+++ Server/catetan.js
@@ -43,3 +43,101 @@
 // npx sequelize-cli model:create --name Category --attributes name:string
 
 //kiwkiwkiw
+
+
+// -- model user 
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class User extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   User.init({
+//     email: {
+//      type: DataTypes.STRING,
+//      allowNull:false,
+//      unique: {
+//       args: true,
+//       msg: "Email is already in use!"
+//      },
+//      validate: {
+//       notEmpty: {
+//         msg: "Email cannot be empty"
+//       },
+//       notNull: {
+//         msg: "Email cannot be empty"
+//       },
+//       isEmail: {
+//         msg: "Input must be in email format"
+//       }
+//      }
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       allowNull: false, 
+//       validate: {
+//         notEmpty: {
+//           msg: "Password cannot be empty"
+//         }, 
+//         notNull: {
+//           msg: "Password cannot be empty"
+//         },
+//         len: {
+//           args: [5,20],
+//           msg: "Password min length is 5 "
+//         }
+//       }
+//     },
+//     fullName: {
+//       type:DataTypes.STRING,
+//       allowNull:false,
+//       validate: {
+//         notEmpty: {
+//           msg: "fullName cannot be empty"
+//         },
+//         notNull: {
+//           msg: "fullName cannot be empty"
+//         }
+//       }
+//     },
+//     birthOfDate: {
+//       type:DataTypes.STRING,
+//       allowNull:false,
+//       validate: {
+//         notEmpty: {
+//           msg: "birth Of Date cannot be empty"
+//         },
+//         notNull: {
+//           msg: "birth Of Date cannot be empty"
+//         }
+//       }
+//     },
+//     phoneNumber: {
+//       allowNull: false,
+//       validate: {
+//         notEmpty: {
+//           msg: "phoneNumber cannot be empty"
+//         },
+//         notNull: {
+//           msg: "phoneNumber cannot be empty"
+//         }
+//       }
+//     },
+//     address: DataTypes.STRING,
+//     balance: DataTypes.INTEGER,
+//     role: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'User',
+//   });
+//   return User;
+// };
