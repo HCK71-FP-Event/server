5fc21c6007de4a2d9ce2d9ef3d810acd1390dc9a
zainalalmasyhur <zainalalmasyhur30@gmail.com>
2024-07-09 20:19:48 +0700

    fix:model_hooks, register

--- Server/controllers/userCtrl.js
+++ Server/controllers/userCtrl.js
@@ -1,40 +1,70 @@
-const { User } = require("../models/index")
-const { comparePass } = require("../helpers/bcrypt")
-const { createToken } = require("../helpers/jsonwebtoken")
-
+const { User } = require("../models/index");
+const { comparePass } = require("../helpers/bcrypt");
+const { createToken } = require("../helpers/jsonwebtoken");
 
 class userCtrl {
-    static async register(req, res, next) {
-        try {
-            const user = await User.create(req.body)
-            res.status(201).json({ id: user.id, email: user.email })
-        } catch (error) {
-            next(error)
-            console.log(error);
-        }
+  static async register(req, res, next) {
+    try {
+      const {
+        email,
+        password,
+        fullName,
+        birthOfDate,
+        phoneNumber,
+        address,
+        avatar,
+      } = req.body;
+
+      if (!email) throw { name: "Email Empty" };
+      if (!password) throw { name: "Password Empty" };
+      if (!fullName) throw { name: "fullName Empty" };
+      if (!birthOfDate) throw { name: "birthOfDate Empty" };
+      if (!phoneNumber) throw { name: "phoneNumber Empty" };
+      if (!address) throw { name: "address Empty" };
+      if (!avatar) throw { name: "avatar Empty" };
+
+      let user = await User.create({
+        email,
+        password,
+        fullName,
+        birthOfDate,
+        phoneNumber,
+        address,
+        avatar,
+      });
+
+      res.status(201).json({
+        message: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    } catch (error) {
+      next(error);
+      console.log(error);
     }
+  }
 
-    static async login(req, res, next) {
-        try {
-            const { email, password } = req.body
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
 
-            if (!email) throw { name: "Email Empty" }
-            if (!password) throw { name: "Password Empty" }
+      if (!email) throw { name: "Email Empty" };
+      if (!password) throw { name: "Password Empty" };
 
-            const user = await User.findOne({ where: { email } })
-            if (!user) throw { name: "Invalid Login" }
+      const user = await User.findOne({ where: { email } });
+      if (!user) throw { name: "Invalid Login" };
 
-            const result = comparePass(password, user.password)
-            if (!result) throw { name: "Invalid Login" }
+      const result = comparePass(password, user.password);
+      if (!result) throw { name: "Invalid Login" };
 
-            const token = createToken({ id: user.id })
-            res.status(200).json({ access_token: token })
-
-        } catch (error) {
-            next(error)
-            console.log(error);
-        }
+      const token = createToken({ id: user.id });
+      res.status(200).json({ access_token: token });
+    } catch (error) {
+      next(error);
+      console.log(error);
     }
+  }
 }
 
-module.exports = { userCtrl }
+module.exports = { userCtrl };
