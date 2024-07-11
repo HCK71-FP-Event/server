f06013c9cedb64aea2b05e2e6f40d43a0c36415a
zainalalmasyhur <zainalalmasyhur30@gmail.com>
2024-07-11 11:16:32 +0700

    add validation

--- Server/models/user.js
+++ Server/models/user.js
@@ -13,12 +13,89 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.init(
     {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      fullName: DataTypes.STRING,
-      birthOfDate: DataTypes.STRING,
-      phoneNumber: DataTypes.STRING,
-      address: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          args: true,
+          msg: "Email already in use!",
+        },
+        validate: {
+          notEmpty: {
+            msg: "Email cannot be empty",
+          },
+          notNull: {
+            msg: "Email cannot be empty",
+          },
+          isEmail: {
+            msg: "Input must be in email format",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Password cannot be empty",
+          },
+          notNull: {
+            msg: "Password cannot be empty",
+          },
+          len: {
+            args: [5, 20],
+            msg: "Password min length is 5",
+          },
+        },
+      },
+      fullName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Name cannot be empty",
+          },
+          notNull: {
+            msg: "Name cannot be empty",
+          },
+        },
+      },
+      birthOfDate: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Birth Date cannot be empty",
+          },
+          notNull: {
+            msg: "Birth Date cannot be empty",
+          },
+        },
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Phone number cannot be empty",
+          },
+          notNull: {
+            msg: "Phone number cannot be empty",
+          },
+        },
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Address cannot be empty",
+          },
+          notNull: {
+            msg: "Address cannot be empty",
+          },
+        },
+      },
       avatar: DataTypes.STRING,
       role: DataTypes.STRING,
     },
