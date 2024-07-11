0267e32c81f24a34cc4048faa2dfb27c0a3724b8
unknown <tizanyb@gmail.com>
2024-07-11 16:00:03 +0700

    Fix: testing fixed

--- Server/__test__/user.test.js
+++ Server/__test__/user.test.js
@@ -11,7 +11,7 @@ const { queryInterface } = sequelize
 let access_token
 
 beforeAll(async () => {
-    const users = require("../data/user.json").map((el)=> {
+    const users = require("../data/user.json").map((el) => {
         el.createdAt = el.updatedAt = new Date()
 
         el.password = hashPassword(el.password)
@@ -36,34 +36,44 @@ afterAll(async () => {
         cascade: true,
         restartIdentity: true
     })
- 
+
 })
-describe("GET /user/:id", ()=> {
-    describe("Success", ()=> {
-        test("Success getting user id by params", async ()=> {
-            const {status, body} = await request(app)
-            .get("/user/1")
-            .set("Authorization", `Bearer ${access_token}`)
+describe("GET /user/:id", () => {
+    describe("Success", () => {
+        test("Success getting user id by params", async () => {
+            const { status, body } = await request(app)
+                .get("/user/1")
+                .set("Authorization", `Bearer ${access_token}`)
 
             expect(status).toBe(200)
             expect(body).toBeInstanceOf(Object)
         })
     })
+    describe("Success", () => {
+        test("Success get currentUser", async () => {
+            const { status, body } = await request(app)
+                .get("/currentUser")
+                .set("Authorization", `Bearer ${access_token}`)
+
+                expect(status).toBe(200)
+                expect(body).toHaveProperty("loginUser")
+        })
+    })
 })
 
-describe("Fail", ()=> {
-    test("Fail to get user id by params", async ()=> {
-        const {status, body} = await request(app)
-        .get("/user/123123123")
-        .set("Authorization", `Bearer ${access_token}`)
+describe("Fail", () => {
+    test("Fail to get user id by params", async () => {
+        const { status, body } = await request(app)
+            .get("/user/123123123")
+            .set("Authorization", `Bearer ${access_token}`)
 
         expect(status).toBe(404)
         expect(body).toHaveProperty("message", "Data Not Found")
     })
-    test("Fail to get User because no access_token", async ()=> {
-        const {status, body} = await request(app)
-        .get("/user/1")
-        .set("Authorization", `Bearer 123123123123213123123`)
+    test("Fail to get User because no access_token", async () => {
+        const { status, body } = await request(app)
+            .get("/user/1")
+            .set("Authorization", `Bearer 123123123123213123123`)
 
         expect(status).toBe(401)
         expect(body).toHaveProperty("message", "Invalid token")
